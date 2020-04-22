b5117da530d0da4a81ada9057f8afd873c2f24b3
jisshub <jissmon476@gmail.com>
2020-04-22 15:44:44 +0530

    input decorator to element property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // define serverElements array
   serverElements = [{type: "server",
                     name:"test server",
                     content: "sample content"}];
 }
+
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  // attach a input decorator to the element property-
+  // will make it vaiable outside this component
+  
+  // <!-- so any component accessing the element property will be 
+  // able to use the element property of ServerComponent class-->
+  @Input()
   element: {type:string, name:string, content:string};
   constructor() { }
 
